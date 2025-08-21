a66594bd15f9e87e8161d93433026d4df58e66a1
akki250 <70622004+akki250@users.noreply.github.com>
2025-08-21 14:40:49 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCT2OEtKuthnM9bBQPG_eRowdHvfGme16w",
+  authDomain: "sta1-16522.firebaseapp.com",
+  databaseURL: "https://sta1-16522-default-rtdb.firebaseio.com",
+  projectId: "sta1-16522",
+  storageBucket: "sta1-16522.appspot.com",
+  messagingSenderId: "1024853989447",
+  appId: "1:1024853989447:web:7923ecac8d73ec68ce7962"
+};
---
+++ google-config.js
@@ -0,0 +1,9 @@
+// Google OAuth Configuration
+// Replace these with your actual Google OAuth client IDs
+
+export const googleConfig = {
+  androidClientId: 'YOUR_ANDROID_CLIENT_ID',
+  iosClientId: 'YOUR_IOS_CLIENT_ID',
+  webClientId: 'YOUR_WEB_CLIENT_ID',
+  scopes: ['profile', 'email'],
+};
